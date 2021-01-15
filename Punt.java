2036de05a0c24145e8679d574b4c9de3abe13036
juancarlos <juankigc01@gmail.com>
2021-01-15 13:30:00 +0100

    hola

---
+++ Punt.java
@@ -0,0 +1,19 @@
+package Ejercicio1;
+
+public class Punt {
+	
+	public static void main(String[] args){
+		Punto punto1 = new Punto();
+		punto1.x =5;
+		punto1.y =0;
+		System.out.println(punto1.x +","+ punto1.y*1);
+		Punto punto2 = new Punto();
+		punto2.x =10;
+		punto2.y =10;
+		System.out.println(punto2.x +","+ punto2.y);
+		Punto punto3 = new Punto();
+		punto3.x =-3;
+		punto3.y =7;
+		System.out.println(punto3.x +","+ punto3.y);
+	}
+}
---
+++ Punto.java
@@ -0,0 +1,6 @@
+package Ejercicio1;
+
+public class Punto {
+int x;
+int y;
+}
